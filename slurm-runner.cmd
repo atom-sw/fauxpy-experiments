5cb5cad5dc8faf85ee1a734ce16a8a0f08da0b43
caf <c.a.furia@gmail.com>
2022-12-22 18:20:13 +0100

    Update cmd by setting permissions.

--- slurm-runner.cmd
+++ slurm-runner.cmd
@@ -1,1 +1,1 @@
-for f in scripts/*.sh; do fn="${f/#scripts\//}"; echo "$fn"; cp "$f" .; slurp --scratch --submit SBATCH --workdir "/home/furia/fauxpy-experiments" "$fn"; done
+for f in scripts/*.sh; do fn="${f/#scripts\//}"; echo "$fn"; cp "$f" .; chmod u+x "$fn"; slurp --scratch --submit SBATCH --workdir "/home/furia/fauxpy-experiments" "$fn"; done
