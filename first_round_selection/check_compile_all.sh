e8298a97b6d93020fe89bb47152da171f94ca802
Moe <rezaalipour.mohammad@gmail.com>
2023-01-10 19:43:55 +0100

    Change compile checker bash script and add compile log files

--- first_round_selection/check_compile_all.sh
+++ first_round_selection/check_compile_all.sh
@@ -6,11 +6,20 @@ set -e
 # Constants
 #--------------------------------------------
 INFO_DIR="info"
+COMPILE_LOG_DIR="compile_log"
 
 # Execution commands
 #--------------------------------------------
 
+if [ -d "$COMPILE_LOG_DIR" ]; then
+  echo "Removing the previous compile log results..."
+  rm -rf "$COMPILE_LOG_DIR"
+fi
+
+mkdir "$COMPILE_LOG_DIR"
+
 for filename in "$INFO_DIR"/*.json; do
-  python check_compile.py "$filename"
+  log_file_name="${filename:5}.log"
+  python check_compile.py "$filename" 2>&1 | tee "$COMPILE_LOG_DIR"/"$log_file_name"
 done
 
