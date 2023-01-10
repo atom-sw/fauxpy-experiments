9eb33a312586743d527744326e536d902834e1f4
Moe <rezaalipour.mohammad@gmail.com>
2023-01-10 20:31:09 +0100

    Update first round selection check_compile_all.sh and check_target_tests_all.sh

--- first_round_selection/check_target_tests_all.sh
+++ first_round_selection/check_target_tests_all.sh
@@ -6,11 +6,20 @@ set -e
 # Constants
 #--------------------------------------------
 INFO_DIR="info"
+OUTPUT_LOG_DIR="target_tests_log"
 
 # Execution commands
 #--------------------------------------------
 
+if [ -d "$OUTPUT_LOG_DIR" ]; then
+  echo "Removing the previous target tests log results..."
+  rm -rf "$OUTPUT_LOG_DIR"
+fi
+
+mkdir "$OUTPUT_LOG_DIR"
+
 for filename in "$INFO_DIR"/*.json; do
-  python check_target_tests.py "$filename"
+  log_file_name="${filename:5}.log"
+  python check_target_tests.py "$filename" 2>&1 | tee "$OUTPUT_LOG_DIR"/"$log_file_name"
 done
 
