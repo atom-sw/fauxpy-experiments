99254a163f50ec640e7bb6f1b018ad86875720f2
Moe <rezaalipour.mohammad@gmail.com>
2023-02-09 19:31:40 +0100

    Update generate_ground_truth_info.py up to extracting patch parts of a single changed file

--- first_round_selection/generate_ground_truth_info.py
+++ first_round_selection/generate_ground_truth_info.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 from typing import Tuple, List
 
@@ -7,7 +8,28 @@ CORRECT = {}
 PATCH_INFO_FILE_NAME: str = "ground_truth_info.json"
 
 
+def get_patch_parts(patch) -> List[List[str]]:
+    patch_parts = []
+    patch_lines = patch.split("\n")
+    part_lines = []
+    for patch_line in patch_lines:
+        meta_line_list = re.findall(r"@@.*@@", patch_line)
+        if len(meta_line_list) > 0:
+            if len(part_lines) > 0:
+                patch_parts.append(part_lines)
+            part_lines = []
+        part_lines.append(patch_line)
+    patch_parts.append(part_lines)
+
+    return patch_parts
+
+
 def get_file_ground_truth(patch: str) -> Tuple[List[int], List[int]]:
+    patch_parts = get_patch_parts(patch)
+    for patch_part in patch_parts:
+        meta_info_line = patch_part[0]
+        lines = patch_part[1:]
+        print(lines)
     return [], []
 
 
