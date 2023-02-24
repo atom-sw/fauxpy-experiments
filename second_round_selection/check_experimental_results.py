17cfb5270a969b39a9ed20c7d2c885dd334aa360
Moe <rezaalipour.mohammad@gmail.com>
2023-02-24 12:08:23 +0100

    Add classes to manage paths and scripts to the second round selection directory

--- second_round_selection/check_experimental_results.py
+++ second_round_selection/check_experimental_results.py
@@ -1,17 +1,32 @@
 from pathlib import Path
+from typing import List
 
-import common
+from common import ScriptItem, PathManager
 
-RESULTS_PATH_FILE_NAME = "results_path.json"
+PATH_ITEMS_FILE_NAME = "path_items.json"
 
 
-def load_results_path(filename):
-    results_path_object = common.load_json_to_dictionary(RESULTS_PATH_FILE_NAME)
-    return Path(results_path_object["RESULTS_PATH"])
+def load_script_items(scripts_path: Path) -> List[ScriptItem]:
+    script_items = []
+    for script_path in scripts_path.iterdir():
+        script_object = ScriptItem(str(script_path.name))
+        script_items.append(script_object)
+    script_items.sort(key=lambda x: x.get_experiment_id())
+    return script_items
 
 
 def main():
-    results_path = load_results_path(RESULTS_PATH_FILE_NAME)
+    path_manager = PathManager(PATH_ITEMS_FILE_NAME)
+
+    scripts_path = path_manager.get_scripts_path()
+    results_path = path_manager.get_results_path()
+
+    script_items = load_script_items(scripts_path)
+    # result_items = load_result_items(results_path)
+
+    for script_item in script_items:
+        print(script_item)
+
     x = 1
 
 
--- second_round_selection/common.py
+++ second_round_selection/common.py
@@ -3,6 +3,62 @@ from pathlib import Path
 from typing import Any
 
 
+class PathManager:
+    def __init__(self, path_items_file_path: str):
+        self._path_items_file_path = path_items_file_path
+        self._results_path, self._scripts_path = self.load_path_items()
+
+    def load_path_items(self):
+        results_path_object = load_json_to_dictionary(self._path_items_file_path)
+        results_path = Path(results_path_object["RESULTS_PATH"])
+        scripts_path = Path(results_path_object["SCRIPTS_PATH"])
+        return results_path, scripts_path
+
+    def get_results_path(self) -> Path:
+        return self._results_path
+
+    def get_scripts_path(self) -> Path:
+        return self._scripts_path
+
+
+class ScriptItem:
+    def __init__(self, script_name: str):
+        self.script_name = script_name
+
+        (self._experiment_id,
+         self._timeout,
+         self._memory,
+         self._project_name,
+         self._bug_num,
+         self._family,
+         self._granularity) = self._load_info_from_name()
+
+    def _load_info_from_name(self) -> tuple[int, int, int, str, int, str, str]:
+        file_name = self.script_name.split(".")[0]
+        name_parts = file_name.split("_")
+        experiment_id = int(name_parts[0])
+        timeout = int(name_parts[1][:-1])
+        memory = int(name_parts[2][:-1])
+        project_name = name_parts[3]
+        bug_num = int(name_parts[4])
+        family = name_parts[5]
+        granularity = name_parts[6]
+        return experiment_id, timeout, memory, project_name, bug_num, family, granularity
+
+    def __str__(self):
+        return (f"{self.script_name}, "
+                f"{self._experiment_id}, "
+                f"{self._timeout}, "
+                f"{self._memory}, "
+                f"{self._project_name}, "
+                f"{self._bug_num}, "
+                f"{self._family}, "
+                f"{self._granularity}")
+
+    def get_experiment_id(self) -> int:
+        return self._experiment_id
+
+
 def load_json_to_dictionary(file_path: str):
     with open(file_path) as file:
         data_dict = json.load(file)
