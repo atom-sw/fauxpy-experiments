90ec37dcc8b019b54f0e9bb47497c2b50c8fcf8e
Moe <rezaalipour.mohammad@gmail.com>
2023-03-12 19:01:34 +0100

    Modify metric computation to compute results for function granularity

--- metric_computation/compute_all.py
+++ metric_computation/compute_all.py
@@ -3,12 +3,7 @@ from result_manager import get_result_manager
 
 def main():
     result_manager = get_result_manager()
-    result_manager.compute_all_metrics_for_all()
-    result_manager.save_all_metrics_for_all()
-    # csv_score_items = result_manager.get_all_csv_score_items()
-    #
-    # for item in csv_score_items:
-    #     print(item, item.get_experiment_time_seconds())
+    result_manager.perform()
 
 
 if __name__ == '__main__':
--- metric_computation/result_manager.py
+++ metric_computation/result_manager.py
@@ -21,64 +21,94 @@ class ResultManager:
         self._size_counts_dict = size_counts_dict
 
     def perform(self):
-        self._compute_all_metrics_for_granularity(self._statement_csv_score_items)
-        # self._compute_all_metrics_for_granularity(self._function_csv_score_items)
+        self._compute_all_metrics_for(self._statement_csv_score_items)
+        self._save_all_metrics_for(self._statement_csv_score_items)
+        self._compute_all_metrics_for(self._function_csv_score_items)
+        self._save_all_metrics_for(self._function_csv_score_items)
 
-    def _compute_all_metrics_for_granularity(self, csv_score_items: List[CsvScoreItem]):
+    def _compute_all_metrics_for(self, csv_score_items: List[CsvScoreItem]):
         for item in csv_score_items:
             e_inspect, exam_score = self._compute_literature_metrics_for_csv_item(item)
             metric_val = MetricVal(item.get_experiment_time_seconds(), e_inspect, exam_score)
             item.set_metric_val(metric_val)
 
-    def get_all_csv_score_items(self):
-        return self._statement_csv_score_items
-
-    # Call this method after calling compute_all_metrics_for_all.
-    def save_all_metrics_for_all(self):
-        technique_statement_csv_items = {}
+    def _save_all_metrics_for(self, csv_score_items: List[CsvScoreItem]):
+        technique_csv_items = {}
         for item in FLTechnique:
-            technique_statement_csv_items[item.name] = self._get_all_csv_items_for(FLTechnique(item.value),
-                                                                                   FLGranularity.Statement)
+            technique_csv_items[item.name] = self._get_all_csv_items_for(FLTechnique(item.value),
+                                                                         csv_score_items)
 
         overall_results_header = ["technique", "experiment_time_seconds", "@1", "@1%", "@3", "@3%", "@5", "@5%", "@10",
                                   "@10%", "exam_score"]
-        technique_statement_overall_table = [overall_results_header]
-        for technique_name, csv_items in technique_statement_csv_items.items():
-            technique_statement_detailed_table = self._get_technique_detailed_results_table(csv_items)
-            technique_statement_detailed_file_name = f"detailed_{technique_name}_{FLGranularity.Statement.name}.csv"
-            file_manager.save_csv_to_output_dir(technique_statement_detailed_table, technique_statement_detailed_file_name)
+        technique_overall_table = [overall_results_header]
+        for technique_name, csv_items in technique_csv_items.items():
+            technique_detailed_table = self._get_technique_detailed_results_table(csv_items)
+            technique_detailed_file_name = f"detailed_{csv_score_items[0].get_granularity().name}_{technique_name}.csv"
+            file_manager.save_csv_to_output_dir(technique_detailed_table,
+                                                technique_detailed_file_name)
 
-            technique_statement_overall_row = self._get_technique_overall_results_row(technique_name, csv_items)
-            technique_statement_overall_table.append(technique_statement_overall_row)
+            technique_overall_row = self._get_technique_overall_results_row(technique_name, csv_items)
+            technique_overall_table.append(technique_overall_row)
 
-        technique_statement_overall_file_name = f"overall_{FLGranularity.Statement.name}.csv"
-        file_manager.save_csv_to_output_dir(technique_statement_overall_table, technique_statement_overall_file_name)
+        technique_statement_overall_file_name = f"overall_{csv_score_items[0].get_granularity().name}.csv"
+        file_manager.save_csv_to_output_dir(technique_overall_table, technique_statement_overall_file_name)
 
     def _compute_e_inspect_for_csv_score_item(self, csv_score_item: CsvScoreItem) -> float:
         bug_key = f"{csv_score_item.get_project_name()}:{csv_score_item.get_bug_number()}"
-        bug_ground_truth_dict = self._ground_truth_info_dict[bug_key]
-        bug_line_count = self._size_counts_dict[bug_key]["LINE_COUNT"]
-        e_inspect_object = EInspect(csv_score_item.get_scored_entities(), bug_line_count, bug_ground_truth_dict)
-        e_inspect_value = e_inspect_object.get_e_inspect()
+        if csv_score_item.get_granularity() == FLGranularity.Statement:
+            bug_line_count = self._size_counts_dict[bug_key]["LINE_COUNT"]
+            buggy_lines_list = self._get_ground_truth_buggy_line_names(bug_key)
+            assert len(buggy_lines_list) > 0
+            e_inspect_object = EInspect(csv_score_item.get_scored_entities(),
+                                        bug_line_count,
+                                        buggy_lines_list)
+            e_inspect_value = e_inspect_object.get_e_inspect()
+        elif csv_score_item.get_granularity() == FLGranularity.Function:
+            function_count = self._size_counts_dict[bug_key]["FUNCTION_COUNT"]
+            buggy_functions_list = self._get_ground_truth_buggy_function_names(bug_key)
+            if len(buggy_functions_list) == 0:
+                # If none of buggy lines in the ground truth
+                # info are functions, E_Inspect is the number
+                # of functions within the source code because
+                # the technique cannot find it and the user has to
+                # go through all functions.
+                # This one does not happen for statement granularity
+                # as we have already removed buggy versions with
+                # empty ground truths.
+                e_inspect_value = function_count
+            else:
+                e_inspect_object = EInspect(csv_score_item.get_scored_entities(),
+                                            function_count,
+                                            buggy_functions_list)
+                e_inspect_value = e_inspect_object.get_e_inspect()
+        else:
+            raise Exception()
 
         return e_inspect_value
 
     def _compute_exam_score_for_csv_score_item(self, csv_score_item, e_inspect: float) -> float:
         bug_key = f"{csv_score_item.get_project_name()}:{csv_score_item.get_bug_number()}"
-        bug_line_count = self._size_counts_dict[bug_key]["LINE_COUNT"]
-        exam_score = e_inspect / bug_line_count
+        if csv_score_item.get_granularity() == FLGranularity.Statement:
+            entity_count_in_project = self._size_counts_dict[bug_key]["LINE_COUNT"]
+        elif csv_score_item.get_granularity() == FLGranularity.Function:
+            entity_count_in_project = self._size_counts_dict[bug_key]["FUNCTION_COUNT"]
+        else:
+            raise Exception()
 
+        exam_score = e_inspect / entity_count_in_project
         return exam_score
 
-    def _compute_literature_metrics_for_csv_item(self, csv_score_item) -> Tuple[float, float]:
+    def _compute_literature_metrics_for_csv_item(self, csv_score_item: CsvScoreItem) -> Tuple[float, float]:
         e_inspect = self._compute_e_inspect_for_csv_score_item(csv_score_item)
         exam_score = self._compute_exam_score_for_csv_score_item(csv_score_item, e_inspect)
         return e_inspect, exam_score
 
-    def _get_all_csv_items_for(self, tech: FLTechnique, granularity: FLGranularity) -> List[CsvScoreItem]:
+    @staticmethod
+    def _get_all_csv_items_for(tech: FLTechnique,
+                               csv_score_items: List[CsvScoreItem]) -> List[CsvScoreItem]:
         technique_csv_items = []
-        for item in self._statement_csv_score_items:
-            if item.get_technique() == tech and item.get_granularity() == granularity:
+        for item in csv_score_items:
+            if item.get_technique() == tech:
                 technique_csv_items.append(item)
 
         technique_csv_items.sort(key=lambda x: (x.get_project_name(), x.get_bug_number()))
@@ -131,19 +161,43 @@ class ResultManager:
 
         return technique_result
 
+    def _get_ground_truth_buggy_line_names(self, bug_key: str) -> List[str]:
+        buggy_entity_names = []
+        bug_ground_truth = self._ground_truth_info_dict[bug_key]
+        for module_item in bug_ground_truth:
+            module_name = module_item["FILE_NAME"]
+            entity_items = module_item["LINES"] + module_item["EXTENDED_LINES"]
+            for entity_item in entity_items:
+                entity_name = f"{module_name}::{entity_item}"
+                buggy_entity_names.append(entity_name)
+
+        return buggy_entity_names
+
+    def _get_ground_truth_buggy_function_names(self, bug_key: str) -> List[str]:
+        buggy_entity_names = []
+        bug_ground_truth = self._ground_truth_info_dict[bug_key]
+        for module_item in bug_ground_truth:
+            module_name = module_item["FILE_NAME"]
+            entity_items = module_item["FUNCTIONS"] + module_item["EXTENDED_FUNCTIONS"]
+            for entity_item in entity_items:
+                entity_name = f"{module_name}::{entity_item}"
+                buggy_entity_names.append(entity_name)
+
+        return buggy_entity_names
+
 
 def get_result_manager():
     path_manager = file_manager.PathManager()
     csv_score_item_load_manager = CsvScoreItemLoadManager(path_manager.get_results_path())
     statement_csv_score_items = csv_score_item_load_manager.load_csv_score_items()
-    file_manager.save_score_items_to_given_directory_path(path_manager.get_statement_csv_score_directory_path(),
-                                                          statement_csv_score_items)
+    # file_manager.save_score_items_to_given_directory_path(path_manager.get_statement_csv_score_directory_path(),
+    #                                                       statement_csv_score_items)
 
     csv_score_item_function_granularity_manager = CsvScoreItemFunctionGranularityManager(statement_csv_score_items,
                                                                                          path_manager.get_workspace_path())
     function_csv_score_items = csv_score_item_function_granularity_manager.get_function_csv_score_items()
-    file_manager.save_score_items_to_given_directory_path(path_manager.get_function_csv_score_directory_path(),
-                                                          function_csv_score_items)
+    # file_manager.save_score_items_to_given_directory_path(path_manager.get_function_csv_score_directory_path(),
+    #                                                       function_csv_score_items)
 
     ground_truth_info = file_manager.load_json_to_dictionary(path_manager.get_ground_truth_file_name())
     size_counts_info = file_manager.load_json_to_dictionary(path_manager.get_size_counts_file_name())
