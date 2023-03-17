a783907f361627a39b22811fbc1b39554d2977e0
Moe <rezaalipour.mohammad@gmail.com>
2023-03-17 20:49:08 +0100

    Finish class TechniqueBugSvCompOverallScore

--- metric_computation/sv_comp_score.py
+++ metric_computation/sv_comp_score.py
@@ -1,8 +1,7 @@
 from typing import List, Dict
 
 from entity_type import ScoredStatement
-from literature_metrics import EInspectBase
-from our_base_types import DistanceBase
+from our_base_types import DistanceBase, TechniqueBugOverallBase
 
 
 class SvCompScoreForBug(DistanceBase):
@@ -38,6 +37,7 @@ class SvCompScoreForBug(DistanceBase):
                            program_location: ScoredStatement) -> float:
         """
         The score S(L, L_2) between two program locations L and L_2.
+        In this implementation, the first one must be a line in ground truth.
         """
 
         d_distance = self._get_line_to_line_distance(buggy_line_name, program_location)
@@ -48,16 +48,40 @@ class SvCompScoreForBug(DistanceBase):
             return -1 * d_distance
 
 
-class TechniqueBugSvCompOverallScore:
+# TODO: what to do if the list is empty?
+class TechniqueBugSvCompOverallScore(TechniqueBugOverallBase):
     def __init__(self,
                  sv_comp_score_bug_obj: SvCompScoreForBug,
                  program_locations: List[ScoredStatement],
                  e_inspect: float):
-        self._sv_comp_score_bug_obj = sv_comp_score_bug_obj
-        self._program_locations = program_locations
-        self._e_inspect = e_inspect
-        self._e_inspect_base = EInspectBase(program_locations,
-                                            sv_comp_score_bug_obj.get_buggy_line_names())
+        super().__init__(sv_comp_score_bug_obj,
+                         program_locations,
+                         e_inspect)
 
-    def get_sv_comp_overall_score(self):
-        pass
+    def get_sv_comp_overall_score(self) -> float:
+        # M(f, b) counts from 1, not 0.
+        m_of_technique_and_bug = self._get_m_of_technique_and_bug()
+
+        sv_comp_best_score_list = []
+
+        for index in range(0, m_of_technique_and_bug):
+            current_prog_location = self._program_locations[index]
+            start_index, end_index = self._e_inspect_base.get_tied_range(current_prog_location.get_score())
+            k = index + 1  # counts from 1, not 0.
+            if start_index == end_index:
+                # not in tie
+                # S_b(L_k) − k − 1
+                best_score = self._distance_base.get_value(current_prog_location) - k - 1
+                sv_comp_best_score_list.append(best_score)
+            else:
+                # in tie
+                value_tie_average = self._get_average_value_of_tie(start_index, end_index)
+                best_score = value_tie_average - k - 1
+                sv_comp_best_score_list.append(best_score)
+
+        if len(sv_comp_best_score_list) == 0:
+            return -10
+        else:
+            sv_comp_overall_score = max(sv_comp_best_score_list)
+
+        return sv_comp_overall_score
