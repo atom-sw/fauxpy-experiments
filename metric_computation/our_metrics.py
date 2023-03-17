b64a7ff16b9615645eb891c7614bb3695339a1cd
Moe <rezaalipour.mohammad@gmail.com>
2023-03-17 20:53:28 +0100

    Refactor our_metrics.py to calculate sv_comp_overall_score and fix a typo in module name

--- metric_computation/our_metrics.py
+++ metric_computation/our_metrics.py
@@ -14,20 +14,27 @@ line number (within m_k).
 """
 
 
-def compute_our_metrics(program_locations: List[ScoredStatement],
-                        buggy_line_names: List[str],
-                        buggy_module_sizes: Dict[str, int],
-                        e_inspect: float):
+def get_cumulative_distance(program_locations: List[ScoredStatement],
+                            buggy_line_names: List[str],
+                            buggy_module_sizes: Dict[str, int],
+                            e_inspect: float):
     distance_to_bug_obj = DistanceToBug(buggy_line_names, buggy_module_sizes)
     technique_bug_cumulative_distance_obj = TechniqueBugCumulativeDistance(distance_to_bug_obj,
                                                                            program_locations,
                                                                            e_inspect)
     cumulative_distance = technique_bug_cumulative_distance_obj.get_cumulative_distance()
 
+    return cumulative_distance
+
+
+def get_sv_comp_overall_score(program_locations: List[ScoredStatement],
+                              buggy_line_names: List[str],
+                              buggy_module_sizes: Dict[str, int],
+                              e_inspect: float):
     sv_comp_score_bug_obj = SvCompScoreForBug(buggy_line_names, buggy_module_sizes)
     technique_bug_sv_comp_overall_score_obj = TechniqueBugSvCompOverallScore(sv_comp_score_bug_obj,
                                                                              program_locations,
                                                                              e_inspect)
     sv_comp_overall_score = technique_bug_sv_comp_overall_score_obj.get_sv_comp_overall_score()
 
-    return cumulative_distance, 0
+    return sv_comp_overall_score
