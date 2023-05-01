d52119d08ca7b21c219cff44893b3a1b24cdbe6e
Moe <rezaalipour.mohammad@gmail.com>
2023-05-01 10:54:58 +0200

    Modify metric computation to calculate AVG techniques

--- metric_computation/average_fault_localization.py
+++ metric_computation/average_fault_localization.py
@@ -2,18 +2,16 @@ from typing import Dict, Tuple, List, Optional
 
 import mathematics
 from csv_score_load_manager import CsvScoreItem, FLTechnique, FLGranularity
-from entity_type import ScoredStatement
-from score_reassignment import assign_new_scores_to_list_statement
+from entity_type import ScoredStatement, ScoredEntity, ScoredFunction, ScoredModule
 
 
 class AverageFaultLocalization:
-    def __init__(self,
-                 all_csv_score_items: List[CsvScoreItem],
-                 include_all_technique_scores: bool):
+    def __init__(self, all_csv_score_items: List[CsvScoreItem]):
         for item in all_csv_score_items:
             assert item.get_bug_key() == all_csv_score_items[0].get_bug_key()
+            assert item.get_granularity() == all_csv_score_items[0].get_granularity()
+        self._granularity = all_csv_score_items[0].get_granularity()
         self._all_csv_score_items = all_csv_score_items
-        self._include_all_technique_scores = include_all_technique_scores
         self._is_mutable = all_csv_score_items[0].get_is_mutable_bug()
         self._is_predicate = all_csv_score_items[0].get_is_predicate()
         self._is_crashing = all_csv_score_items[0].get_is_crashing()
@@ -23,7 +21,7 @@ class AverageFaultLocalization:
         self._technique_min_max = self._get_technique_min_max()
 
     def _get_technique_min_max(self) -> Dict[FLTechnique, Tuple[float, float]]:
-        def get_min_max(scored_entities: List[ScoredStatement]) -> Tuple[Optional[float], Optional[float]]:
+        def get_min_max(scored_entities: List[ScoredEntity]) -> Tuple[Optional[float], Optional[float]]:
             all_scores = [x.get_score() for x in scored_entities]
             if len(all_scores) == 0:
                 return None, None
@@ -51,34 +49,30 @@ class AverageFaultLocalization:
         normalized_score = mathematics.get_normalized_value(min_value, max_value, score_value)
         return normalized_score
 
-    def get_average_fl_statement_csv_score_item(self) -> CsvScoreItem:
-        scored_statement_list = []
+    def get_average_fl_csv_score_item(self) -> CsvScoreItem:
+        scored_entity_list = []
 
-        all_statement_names = self._get_all_scored_statements()
-        for statement_name in all_statement_names:
-            current_statement_normalized_scores = []
+        all_entity_names = self._get_all_scored_entities()
+        for entity_name in all_entity_names:
+            current_entity_normalized_scores = []
             for csv_score_item in self._all_csv_score_items:
-                current_technique_score = self._get_statement_score_for_technique(statement_name, csv_score_item)
+                current_technique_score = self._get_entity_score_for_technique(entity_name, csv_score_item)
                 current_normalized_score = self._get_normalized_scored_for_technique(current_technique_score,
                                                                                      csv_score_item.get_technique())
-                current_statement_normalized_scores.append((csv_score_item.get_technique(), current_normalized_score))
-            average_score = self._get_average_score(current_statement_normalized_scores)
-            statement_name_parts = statement_name.split("::")
-            file_name = statement_name_parts[0]
-            line_number = int(statement_name_parts[1])
-            current_scored_statement = ScoredStatement(file_name, average_score, line_number)
-            scored_statement_list.append(current_scored_statement)
+                current_entity_normalized_scores.append((csv_score_item.get_technique(), current_normalized_score))
+            average_score = self._get_average_score(current_entity_normalized_scores)
 
-        # reassigned_score_statement_list = assign_new_scores_to_list_statement(scored_statement_list)
+            current_scored_entity = self._get_scored_entity_from_name(entity_name, average_score)
+            scored_entity_list.append(current_scored_entity)
 
-        scored_statement_list.sort(key=lambda x: x.get_score(), reverse=True)
+        scored_entity_list.sort(key=lambda x: x.get_score(), reverse=True)
         csv_score_item = CsvScoreItem(None,
                                       None,
                                       self._project_name,
                                       self._bug_number,
                                       FLTechnique.Average,
-                                      FLGranularity.Statement,
-                                      scored_statement_list,
+                                      self._granularity,
+                                      scored_entity_list,
                                       -1)
         csv_score_item.set_is_crashing(self._is_crashing)
         csv_score_item.set_is_predicate(self._is_predicate)
@@ -88,30 +82,31 @@ class AverageFaultLocalization:
 
         return csv_score_item
 
-    def _get_all_scored_statements(self) -> List[str]:
-        all_statements = []
+    def _get_all_scored_entities(self) -> List[str]:
+        all_entities = []
         for csv_score_item in self._all_csv_score_items:
-            for scored_statement in csv_score_item.get_scored_entities():
-                if scored_statement.get_entity_name() not in all_statements:
-                    all_statements.append(scored_statement.get_entity_name())
-        return all_statements
+            for scored_entities in csv_score_item.get_scored_entities():
+                if scored_entities.get_entity_name() not in all_entities:
+                    all_entities.append(scored_entities.get_entity_name())
+        return all_entities
 
     @staticmethod
-    def _get_statement_score_for_technique(statement_name: str,
-                                           csv_score_item: CsvScoreItem) -> Optional[float]:
-        for scored_statement in csv_score_item.get_scored_entities():
-            if scored_statement.get_entity_name() == statement_name:
-                return scored_statement.get_score()
+    def _get_entity_score_for_technique(entity_name: str,
+                                        csv_score_item: CsvScoreItem) -> Optional[float]:
+        for scored_entity in csv_score_item.get_scored_entities():
+            if scored_entity.get_entity_name() == entity_name:
+                return scored_entity.get_score()
 
         return None
 
     @staticmethod
     def _get_average_score(technique_score_list: List[Tuple[FLTechnique, Optional[float]]]) -> float:
-        ochiai_w = 1
-        dstar_w = 1
-        # metallaxis_w = 1
-        # ps_w = 1.5
-        st_w = 2
+        ochiai_w = 3
+        dstar_w = 3
+        metallaxis_w = 2
+        muse_w = 2
+        ps_w = 1
+        st_w = 1
 
         w_sum = 0
         for item in technique_score_list:
@@ -122,12 +117,12 @@ class AverageFaultLocalization:
                 w_sum += ochiai_w * technique_score
             elif item[0] == FLTechnique.DStar:
                 w_sum += dstar_w * technique_score
-            # elif item[0] == FLTechnique.Metallaxis:
-            #     w_sum += metallaxis_w * technique_score
-            # elif item[0] == FLTechnique.Muse:
-            #     w_sum += muse_w * technique_score
-            # elif item[0] == FLTechnique.PS:
-            #     w_sum += ps_w * technique_score
+            elif item[0] == FLTechnique.Metallaxis:
+                w_sum += metallaxis_w * technique_score
+            elif item[0] == FLTechnique.Muse:
+                w_sum += muse_w * technique_score
+            elif item[0] == FLTechnique.PS:
+                w_sum += ps_w * technique_score
             elif item[0] == FLTechnique.ST:
                 w_sum += st_w * technique_score
             else:
@@ -136,3 +131,22 @@ class AverageFaultLocalization:
         average_score = w_sum / len(technique_score_list)
 
         return average_score
+
+    def _get_scored_entity_from_name(self, entity_name, average_score) -> ScoredEntity:
+        entity_name_parts = entity_name.split("::")
+        if self._granularity == FLGranularity.Statement:
+            file_name = entity_name_parts[0]
+            line_number = int(entity_name_parts[1])
+            current_scored_entity = ScoredStatement(file_name, average_score, line_number)
+        elif self._granularity == FLGranularity.Function:
+            # black.py::patch_click::6320::6339
+            file_name = entity_name_parts[0]
+            func_name = entity_name_parts[1]
+            func_range = (int(entity_name_parts[2]), int(entity_name_parts[3]))
+            current_scored_entity = ScoredFunction(file_name, average_score, func_range, func_name)
+        elif self._granularity == FLGranularity.Module:
+            current_scored_entity = ScoredModule(entity_name, average_score)
+        else:
+            raise Exception()
+
+        return current_scored_entity
--- metric_computation/compute_all.py
+++ metric_computation/compute_all.py
@@ -13,6 +13,9 @@ from latex_info_generator import LatexInfo
 from result_manager import ResultManager
 from selected_bugs_types import assign_type_to_selected_bugs
 
+Alfa = "alfa"
+Sbst = "sbst"
+
 
 def are_all_different_in_list(item_list) -> bool:
     if len(item_list) == 0:
@@ -83,39 +86,43 @@ def get_fs_hfl_statement_csv_score_items(fauxpy_statement_csv_score_items,
     return fs_hfl_statement_csv_score_items
 
 
-def get_average_fl_statement_csv_score_items(fauxpy_statement_csv_score_items):
+def get_avg_csv_score_items(fauxpy_csv_score_items, tech_name: str):
     def get_csv_technique(technique: FLTechnique, bug_key: str):
         technique_csv = list(filter(lambda x:
                                     (x.get_technique() == technique and
                                      x.get_bug_key() == bug_key),
-                                    fauxpy_statement_csv_score_items))[0]
+                                    fauxpy_csv_score_items))[0]
         return technique_csv
 
-    average_fl_statement_csv_score_item_list = []
+    average_fl_csv_score_item_list = []
 
     all_ochiai_csv_list = list(
-        filter(lambda x: x.get_technique() == FLTechnique.Ochiai, fauxpy_statement_csv_score_items))
+        filter(lambda x: x.get_technique() == FLTechnique.Ochiai, fauxpy_csv_score_items))
     for ochiai_csv in all_ochiai_csv_list:
         print(ochiai_csv.get_bug_key())
-        tarantula_csv = get_csv_technique(FLTechnique.Tarantula, ochiai_csv.get_bug_key())
         dstar_csv = get_csv_technique(FLTechnique.DStar, ochiai_csv.get_bug_key())
-        metallaxis_csv = get_csv_technique(FLTechnique.Metallaxis, ochiai_csv.get_bug_key())
-        muse_csv = get_csv_technique(FLTechnique.Muse, ochiai_csv.get_bug_key())
-        ps_csv = get_csv_technique(FLTechnique.PS, ochiai_csv.get_bug_key())
         st_csv = get_csv_technique(FLTechnique.ST, ochiai_csv.get_bug_key())
         techniques_csv_list = [
             ochiai_csv,
             dstar_csv,
-            # metallaxis_csv,
-            # muse_csv,
-            # ps_csv,
             st_csv
         ]
-        average_fl = AverageFaultLocalization(techniques_csv_list, False)
-        average_fl_statement_csv_score_item = average_fl.get_average_fl_statement_csv_score_item()
-        average_fl_statement_csv_score_item_list.append(average_fl_statement_csv_score_item)
 
-    return average_fl_statement_csv_score_item_list
+        if tech_name == Alfa:
+            metallaxis_csv = get_csv_technique(FLTechnique.Metallaxis, ochiai_csv.get_bug_key())
+            muse_csv = get_csv_technique(FLTechnique.Muse, ochiai_csv.get_bug_key())
+            ps_csv = get_csv_technique(FLTechnique.PS, ochiai_csv.get_bug_key())
+            techniques_csv_list += [
+                metallaxis_csv,
+                muse_csv,
+                ps_csv,
+            ]
+
+        average_fl = AverageFaultLocalization(techniques_csv_list)
+        average_fl_csv_score_item = average_fl.get_average_fl_csv_score_item()
+        average_fl_csv_score_item_list.append(average_fl_csv_score_item)
+
+    return average_fl_csv_score_item_list
 
 
 def save_detailed(detailed_table: List, tool_name: str, granularity: str, bug_type: str, dir_name: str):
@@ -233,16 +240,19 @@ def calc_fs_hfl_statement_and_save(fs_hfl_statement_csv_score_items, ground_trut
     save_overall(literature_overall_table, "fs_hfl", "statement", dir_name)
 
 
-def calc_average_fl_statement_and_save(average_fl_statement_csv_score_items, ground_truth_info, size_counts_info):
-    average_fl_statement_result_manager = ResultManager(average_fl_statement_csv_score_items,
-                                                        ground_truth_info,
-                                                        size_counts_info,
-                                                        True)
-    literature_detailed_tables, literature_overall_table = average_fl_statement_result_manager.get_metric_results()
-    dir_name = "output_average_fl_statement"
-    file_manager.clean_make_output_dir(dir_name)
-    save_detailed(literature_detailed_tables, "average_fl", "statement", dir_name)
-    save_overall(literature_overall_table, "average_fl", "statement", dir_name)
+def calc_avg_and_save(avg_csv_score_items, ground_truth_info, size_counts_info, tech_name: str):
+    granularity_name = avg_csv_score_items[0].get_granularity().name.lower()
+    our_metric = granularity_name == FLGranularity.Statement
+    avg_result_manager = ResultManager(avg_csv_score_items,
+                                       ground_truth_info,
+                                       size_counts_info,
+                                       our_metric)
+    all_detailed_tables, all_overall_table = avg_result_manager.get_metric_results()
+    dir_name = f"output_avg_{tech_name}_{granularity_name}"
+    file_manager.make_if_not_dir(dir_name)
+    save_detailed(all_detailed_tables, f"avg_{tech_name}", granularity_name, "all", dir_name)
+    save_overall(all_overall_table, f"avg_{tech_name}", granularity_name, "all", dir_name)
+    save_latex_info(all_overall_table, f"avg_{tech_name}", granularity_name, "all")
 
 
 def generate_metrics():
@@ -254,7 +264,7 @@ def generate_metrics():
 
     # file_manager.save_score_items_to_given_directory_path(path_manager.get_statement_csv_score_directory_path(),
     #                                                       fauxpy_statement_csv_score_items)
-    calc_fauxpy_statement_and_save(fauxpy_statement_csv_score_items, ground_truth_info, size_counts_info)
+    # calc_fauxpy_statement_and_save(fauxpy_statement_csv_score_items, ground_truth_info, size_counts_info)
 
     # fauxpy_function_csv_score_items = convert_statement_csv_to_function_csv(path_manager,
     #                                                                         fauxpy_statement_csv_score_items)
@@ -263,7 +273,7 @@ def generate_metrics():
     #                                                       fauxpy_function_csv_score_items)
     # calc_fauxpy_function_and_save(fauxpy_function_csv_score_items, ground_truth_info, size_counts_info)
 
-    # fauxpy_module_csv_score_items = convert_statement_csv_to_module_csv(fauxpy_statement_csv_score_items)
+    fauxpy_module_csv_score_items = convert_statement_csv_to_module_csv(fauxpy_statement_csv_score_items)
     # file_manager.save_score_items_to_given_directory_path(path_manager.get_module_csv_score_directory_path(),
     #                                                       fauxpy_module_csv_score_items)
     # calc_fauxpy_module_and_save(fauxpy_module_csv_score_items, ground_truth_info, size_counts_info)
@@ -277,9 +287,24 @@ def generate_metrics():
     #                                                                         fauxpy_function_csv_score_items,
     #                                                                         fauxpy_module_csv_score_items)
     # calc_fs_hfl_statement_and_save(fs_hfl_statement_csv_score_items, ground_truth_info, size_counts_info)
-    #
-    # average_fl_statement_csv_score_items = get_average_fl_statement_csv_score_items(fauxpy_statement_csv_score_items)
-    # calc_average_fl_statement_and_save(average_fl_statement_csv_score_items, ground_truth_info, size_counts_info)
+
+    # avg_alfa_statement_csv_score_items = get_avg_csv_score_items(fauxpy_statement_csv_score_items, Alfa)
+    # calc_avg_and_save(avg_alfa_statement_csv_score_items, ground_truth_info, size_counts_info, Alfa)
+
+    # avg_sbst_statement_csv_score_items = get_avg_csv_score_items(fauxpy_statement_csv_score_items, Sbst)
+    # calc_avg_and_save(avg_sbst_statement_csv_score_items, ground_truth_info, size_counts_info, Sbst)
+
+    # avg_alfa_function_csv_score_items = get_avg_csv_score_items(fauxpy_function_csv_score_items, Alfa)
+    # calc_avg_and_save(avg_alfa_function_csv_score_items, ground_truth_info, size_counts_info, Alfa)
+
+    # avg_sbst_function_csv_score_items = get_avg_csv_score_items(fauxpy_function_csv_score_items, Sbst)
+    # calc_avg_and_save(avg_sbst_function_csv_score_items, ground_truth_info, size_counts_info, Sbst)
+
+    # avg_alfa_module_csv_score_items = get_avg_csv_score_items(fauxpy_module_csv_score_items, Alfa)
+    # calc_avg_and_save(avg_alfa_module_csv_score_items, ground_truth_info, size_counts_info, Alfa)
+
+    avg_sbst_module_csv_score_items = get_avg_csv_score_items(fauxpy_module_csv_score_items, Sbst)
+    calc_avg_and_save(avg_sbst_module_csv_score_items, ground_truth_info, size_counts_info, Sbst)
 
 
 def generate_combine_fl_data_input():
@@ -331,7 +356,8 @@ def generate_combine_fl_data_input():
         file_manager.save_csv_to_output_dir(qid_lines_csv_table, dir_name,
                                             f"python_{granularity_name}_qid-lines.csv")
         file_manager.save_object_to_json(qid_ground_truth_num_items,
-                                         Path(dir_name) / f"python_{granularity_name}_qid_ground_truth_number_of_items.json")
+                                         Path(
+                                             dir_name) / f"python_{granularity_name}_qid_ground_truth_number_of_items.json")
         file_manager.save_string_to_file(fl_tech_str, Path(dir_name) / "techniques.txt")
         file_manager.save_string_to_file(proj_string, Path(dir_name) / "projects.txt")
 
@@ -453,6 +479,6 @@ def get_bug_statistics():
 
 if __name__ == '__main__':
     # generate_combine_fl_data_input()
-    # generate_metrics()
-    generate_latex_data_information()
+    generate_metrics()
+    # generate_latex_data_information()
     # get_bug_statistics()
