b869a8f3aa174e3f962661b6b2f401907e6457b9
Moe <rezaalipour.mohammad@gmail.com>
2023-05-25 19:00:13 +0200

    Finish make_clean_package.sh

--- make_clean_package.sh
+++ make_clean_package.sh
@@ -3,8 +3,22 @@
 # Exiting when any command fails
 set -e
 
-TEMP_DIRECTORY="/tmp/fauxpy_replication_package"
+PACKAGE_NAME="fauxpy_experiments"
+TEMP_DIRECTORY="/tmp/$PACKAGE_NAME"
 
+CLEAN_PACKAGE_DIR="clean_package"
+
+FILES_TO_REMOVE=(
+"bugsinpy_project_types.csv"
+"curbatch"
+"donezo.sh"
+"faux-in-py.sh"
+"make_clean_package.sh"
+"slurm-runner.cmd"
+".git"
+)
+
+SCRIPT_DIR="$(dirname $(readlink -f "${BASH_SOURCE[0]}"))"
 
 if [ -d "$TEMP_DIRECTORY" ]; then
   rm -rf "$TEMP_DIRECTORY"
@@ -16,26 +30,30 @@ cp -r . "$TEMP_DIRECTORY"
 
 cd "$TEMP_DIRECTORY"
 
-# Remove all .gitignore files
+# Remove all .gitignore files in tmp version
 ALL_GITIGNORE_FILES=$(find -type f -name '.gitignore')
 for item in "${ALL_GITIGNORE_FILES[@]}"; do
     echo "$item"
     rm -rf "$item"
 done
 
-# Remove the following files
-FILES_TO_REMOVE=(
-"bugsinpy_project_types.csv"
-"curbatch"
-"donezo.sh"
-"faux-in-py.sh"
-"make_clean_package.sh"
-"slurm-runner.cmd"
-".git"
-)
-
+# Remove the unwanted files in the tmp version
 for item in "${FILES_TO_REMOVE[@]}"; do
     echo "$item"
     rm -rf "$item"
 done
 
+# Remove CLEAN_PACKAGE_DIR in the tmp version
+if [ -d "$CLEAN_PACKAGE_DIR" ]; then
+  rm -rf "$CLEAN_PACKAGE_DIR"
+fi
+
+cd "$SCRIPT_DIR"
+
+if [ -d "$CLEAN_PACKAGE_DIR" ]; then
+  rm -rf "$CLEAN_PACKAGE_DIR"
+fi
+
+mkdir "$CLEAN_PACKAGE_DIR"
+
+cp -r "$TEMP_DIRECTORY" "$CLEAN_PACKAGE_DIR/$PACKAGE_NAME"
